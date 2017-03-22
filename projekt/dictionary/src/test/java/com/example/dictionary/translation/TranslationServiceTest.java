acbeff96f7e4e519c95a58d5fd827a718d36a055
kubamarchwicki <kuba@marchwicki.pl>
2017-03-22 19:21:24 +0100

    [projekt] fixing TranslationServiceTest with Mockito mock()

--- projekt/dictionary/src/test/java/com/example/dictionary/translation/TranslationServiceTest.java
+++ projekt/dictionary/src/test/java/com/example/dictionary/translation/TranslationServiceTest.java
@@ -1,8 +1,9 @@
 package com.example.dictionary.translation;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.List;
 
@@ -12,18 +13,24 @@ import static org.junit.Assert.assertThat;
 
 public class TranslationServiceTest {
 
-    TranslationService service = new TranslationService(TranslationServiceTest.class.getResource("/words/book.html").toExternalForm());
+    final String ROOT_FOLDER = TranslationServiceTest.class.getResource("/words/").toExternalForm();
+
+    TranslationService service = new TranslationService(ROOT_FOLDER + "/{}.html");
+
+    @Before
+    public void setup() {
+        service.publisher = Mockito.mock(ApplicationEventPublisher.class);
+    }
 
     @Test
     public void should_return_translations_for_word_book() {
         List<DictionaryWord> books = service.getTranslationsForWord("book");
 
         assertThat(books, hasSize(24));
-//        assertThat(books.get(0), equalTo(new DictionaryWord("książka", "book")));
+        assertThat(books.get(0), equalTo(new DictionaryWord("książka", "book")));
     }
 
     @Test
-    @Ignore
     public void should_return_empty_list_for_non_existing_translation() {
         List<DictionaryWord> books = service.getTranslationsForWord("yadayada");
 
