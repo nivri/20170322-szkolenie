b572926abfd63ed32a5047463dd32e5cd5445d5f
kubamarchwicki <kuba@marchwicki.pl>
2017-03-24 10:13:35 +0100

    [sandbox] content JSON

--- projekt/www/src/main/java/com/example/www/controllers/WordsController.java
+++ projekt/www/src/main/java/com/example/www/controllers/WordsController.java
@@ -2,15 +2,14 @@ package com.example.www.controllers;
 
 import com.example.dictionary.translation.DictionaryWord;
 import com.example.dictionary.translation.TranslationService;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class WordsController {
 
     final private TranslationService service;
@@ -19,10 +18,9 @@ public class WordsController {
         this.service = service;
     }
 
-    @RequestMapping(path = "/search", method = RequestMethod.GET)
-    public void searchForWord(@RequestParam("word") String word) {
-        List<DictionaryWord> wordList = service.getTranslationsForWord(word);
-        System.out.println("wordList = " + wordList);
+    @GetMapping(path = "/search", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<DictionaryWord> searchForWord(@RequestParam("word") String word) {
+        return service.getTranslationsForWord(word);
     }
 
 }
